f21097cc19fdaf8554b8dffaeb572fc39b8a73fb
J-P Nurmi <jp.nurmi@canonical.com>
2022-05-26 22:58:19 +0200

    HomeController

---
+++ packages/ubuntu_lxd_terminal/lib/home_controller.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lxd_service/lxd_service.dart';
+import 'package:lxd_x/lxd_x.dart';
+import 'package:xterm/xterm.dart';
+
+import 'terminal/terminal_backend.dart';
+import 'terminal/terminal_settings.dart';
+import 'terminal/terminal_state.dart';
+
+final homeController = ChangeNotifierProvider<HomeController>((ref) {
+  final service = getService<LxdService>();
+  return HomeController(service);
+});
+
+class HomeController extends ChangeNotifier {
+  HomeController(this._service);
+
+  final LxdService _service;
+
+  var _currentIndex = -1;
+  final _terminals = <TerminalState>[];
+
+  int get length => _terminals.length;
+  TerminalState? terminal(int index) => _terminals.elementAtOrNull(index);
+  TerminalState? get currentTerminal => terminal(_currentIndex);
+  Terminal? get currentRunning {
+    return currentTerminal?.whenOrNull(running: (_, terminal) => terminal);
+  }
+
+  int get currentIndex => _currentIndex;
+  set currentIndex(int index) {
+    if (_currentIndex == index) return;
+    _currentIndex = index;
+    notifyListeners();
+  }
+
+  void add([TerminalState terminal = const TerminalState.none()]) {
+    _terminals.add(terminal);
+    currentIndex = _terminals.length - 1;
+  }
+
+  void close() => closeAt(_currentIndex);
+
+  void closeAt(int index) {
+    _terminals.removeAt(index);
+    currentIndex = _currentIndex.clamp(0, _terminals.length - 1);
+  }
+
+  void move(int from, int to) {
+    _terminals.move(from, to);
+    if (_currentIndex == to) {
+      currentIndex = from;
+    } else if (_currentIndex == from) {
+      currentIndex = to;
+    }
+  }
+
+  void next() {
+    currentIndex = _currentIndex % _terminals.length;
+  }
+
+  void prev() {
+    final index = currentIndex - 1;
+    currentIndex = index < 0 ? _terminals.length - 1 : index;
+  }
+
+  Future<void> create(LxdRemoteImage image, [String? name]) async {
+    final create = await _service.createInstance(image: image, name: name);
+    _setState(_currentIndex, TerminalState.loading(create));
+
+    final wait = await _service.waitOperation(create.id);
+    if (wait.statusCode == LxdStatusCode.cancelled.value) {
+      reset();
+    } else {
+      name = create.instances!.single.split('/').last;
+      return start(name);
+    }
+  }
+
+  Future<void> start(String name) async {
+    final start = await _service.startInstance(name);
+    _setState(_currentIndex, TerminalState.loading(start));
+
+    final wait = await _service.waitOperation(start.id);
+    if (wait.statusCode == LxdStatusCode.cancelled.value) {
+      reset();
+    } else {
+      return run(name);
+    }
+  }
+
+  Future<void> run(String name) async {
+    final instance = await _service.getInstance(name);
+    _setState(
+      _currentIndex,
+      TerminalState.running(
+        instance: instance,
+        terminal: Terminal(
+          backend: LxdTerminalBackend(
+            client: _service,
+            instance: instance.name,
+            onExit: reset,
+          ),
+          maxLines: 10000, // TODO: configurable
+          theme: terminalTheme,
+          onTitleChange: (t) {
+            // TODO: update terminal title?
+          },
+        ),
+      ),
+    );
+  }
+
+  void reset() => _setState(_currentIndex, const TerminalState.none());
+
+  void _setState(int index, TerminalState terminal) {
+    if (_terminals[index] == terminal) return;
+    _terminals[index] = terminal;
+    notifyListeners();
+  }
+
+  Future<void> copy() async {
+    final data = ClipboardData(text: currentRunning?.selectedText);
+    return Clipboard.setData(data);
+  }
+
+  Future<void> paste() async {
+    final data = await Clipboard.getData('text/plain');
+    currentRunning?.paste(data?.text ?? '');
+  }
+
+  void selectAll() => currentRunning?.selectAll();
+}
+
+extension ListX<T> on List<T> {
+  T? elementAtOrNull(int index) {
+    if (index < 0 || index >= length) return null;
+    return this[index];
+  }
+
+  void move(int from, int to) {
+    final tmp = this[from];
+    this[from] = this[to];
+    this[to] = tmp;
+  }
+}
--- packages/ubuntu_lxd_terminal/lib/home_page.dart
+++ packages/ubuntu_lxd_terminal/lib/home_page.dart
@@ -5,10 +5,10 @@ import 'package:lxd_service/lxd_service.dart';
 import 'package:movable_tabs/movable_tabs.dart';
 import 'package:native_context_menu/native_context_menu.dart' as n;
 
+import 'home_controller.dart';
 import 'launch_view.dart';
 import 'operations/operation_view.dart';
 import 'terminal/terminal_state.dart';
-import 'terminal/terminal_store.dart';
 import 'terminal/terminal_view.dart';
 
 class HomePage extends ConsumerWidget {
@@ -16,25 +16,8 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final tabs = ref.watch(terminalStore);
-    final current = ref.watch(currentTerminal);
-
-    Future<void> onCopy() async {
-      if (current is! TerminalRunning) return;
-      final data = ClipboardData(text: current.terminal.selectedText);
-      return Clipboard.setData(data);
-    }
-
-    Future<void> onPaste() async {
-      if (current is! TerminalRunning) return;
-      final data = await Clipboard.getData('text/plain');
-      current.terminal.paste(data?.text ?? '');
-    }
-
-    void onSelectAll() {
-      if (current is! TerminalRunning) return;
-      current.terminal.selectAll();
-    }
+    final controller = ref.watch(homeController);
+    final current = controller.currentTerminal ?? const TerminalState.none();
 
     return CallbackShortcuts(
       bindings: {
@@ -43,64 +26,58 @@ class HomePage extends ConsumerWidget {
           control: true,
           shift: true,
           includeRepeats: false,
-        ): ref.read(terminalStore.notifier).add,
-        if (tabs.length > 1)
+        ): controller.add,
+        if (controller.length > 1)
           const SingleActivator(
             LogicalKeyboardKey.keyW,
             control: true,
             shift: true,
             includeRepeats: false,
-          ): ref.read(terminalStore.notifier).close,
+          ): controller.close,
         const SingleActivator(
           LogicalKeyboardKey.pageUp,
           control: true,
-        ): ref.read(terminalStore.notifier).prev,
+        ): controller.prev,
         const SingleActivator(
           LogicalKeyboardKey.pageDown,
           control: true,
-        ): ref.read(terminalStore.notifier).next,
+        ): controller.next,
       },
       child: Focus(
         autofocus: true,
         child: Scaffold(
-          appBar: tabs.length <= 1
+          appBar: controller.length <= 1
               ? null
               : MovableTabBar(
-                  count: tabs.length,
+                  count: controller.length,
                   builder: (context, index) {
                     return MovableTabButton(
-                      selected: index == ref.watch(currentIndex),
-                      onPressed: () =>
-                          ref.read(currentIndex.state).update((_) => index),
-                      onClosed: () =>
-                          ref.read(terminalStore.notifier).closeAt(index),
+                      selected: index == controller.currentIndex,
+                      onPressed: () => controller.currentIndex = index,
+                      onClosed: () => controller.closeAt(index),
                       label: const Text('Terminal'), // TODO: title
                     );
                   },
-                  onMoved: ref.read(terminalStore.notifier).move,
+                  onMoved: controller.move,
                   preferredHeight: Theme.of(context).appBarTheme.toolbarHeight,
                 ),
           body: n.ContextMenuRegion(
             onItemSelected: (dynamic item) => item.action?.call(),
             menuItems: <n.MenuItem>[
               if (current is TerminalRunning)
-                n.MenuItem(title: 'Copy', action: onCopy),
+                n.MenuItem(title: 'Copy', action: controller.copy),
               if (current is TerminalRunning)
-                n.MenuItem(title: 'Paste', action: onPaste),
+                n.MenuItem(title: 'Paste', action: controller.paste),
               if (current is TerminalRunning)
-                n.MenuItem(title: 'Select All', action: onSelectAll),
-              n.MenuItem(
-                  title: 'New Tab',
-                  action: ref.read(terminalStore.notifier).add),
-              if (tabs.length > 1)
-                n.MenuItem(
-                    title: 'Close Tab',
-                    action: ref.read(terminalStore.notifier).close),
+                n.MenuItem(title: 'Select All', action: controller.selectAll),
+              n.MenuItem(title: 'New Tab', action: controller.add),
+              if (controller.length > 1)
+                n.MenuItem(title: 'Close Tab', action: controller.close),
             ],
             child: current.when(
               none: () => LaunchView(
-                onStart: ref.read(terminalStore.notifier).start,
-                onCreate: ref.read(terminalStore.notifier).create,
+                onStart: controller.start,
+                onCreate: controller.create,
                 onDelete: getService<LxdService>().deleteInstance,
                 onStop: getService<LxdService>().stopInstance,
               ),
